926a427e3cdaf291977c04367bfa6046f927ee0f
sbilalx <135545213+sbilalx@users.noreply.github.com>
2026-01-04 07:32:35 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-// WE CHANGED THE VERSION NUMBER HERE TO V3
-const CACHE_NAME = 'lahore-prayers-v3';
+// VERSION V5 (Forces update for new jpg icon)
+const CACHE_NAME = 'lahore-prayers-v5';
 const ASSETS_TO_CACHE = [
   './',
-  './index.html'
+  './index.html',
+  './icon.jpg'  // <--- Updated to match your file
 ];
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting(); // Force this new worker to become active immediately
+  self.skipWaiting(); 
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
@@ -17,7 +18,6 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      // Network First strategy for data, Cache fallback
       return fetch(event.request).catch(() => cachedResponse);
     })
   );
@@ -28,7 +28,6 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
-          console.log("Removing old cache", key);
           return caches.delete(key);
         }
       }));
